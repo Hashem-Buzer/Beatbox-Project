b3527488b7c0bd3b3342dc4cc25bfa2e50886b27
hashem-buzer <hbuzer@gmail.com>
2019-11-08 12:40:56 +0100

    c2

--- assets/main.js
+++ assets/main.js
@@ -13,133 +13,165 @@ var generalVideos = [
 	'<iframe width="600" height="300" src="https://www.youtube.com/embed/bjOcNXivsiA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
 ];
 
-// var challengesVideos = [
-// 	{
-// 		src: '',
-// 		tutorialName: ''
-// 	}
-// ];
+
 
 var beatBoxers = [
 	{
 		name: 'D-low',
 		youtube: 'https://www.youtube.com/channel/UCI2-CSNIq-_GhJOA2ORF9wg/videos',
-		image: ''
+		image: 'assets/images/Dlow.jpg'
 	},
 	{
 		name: 'Codfish',
 		youtube: 'https://www.youtube.com/user/BunnyF1uff/videos',
-		image: ''
+		image: 'assets/images/codfish.jpg'
 	},
 	{
 		name: 'Reeps One',
 		youtube: 'https://www.youtube.com/user/Reepsone/featured',
-		image: ''
+		image: 'assets/images/ReepsOne.jpg'
 	},
 	{
 		name: 'B-Art',
 		youtube: 'https://www.youtube.com/user/BPMxDBeatsPerMinute/featured',
-		image: ''
+		image: 'assets/images/B-art.jpg'
 	},
-	{
-		name: 'Audical',
-		youtube: 'https://www.youtube.com/channel/UCf7ujqQzI9YY5RzzBgpGSbg/featured',
-		image: ''
-	},
-	{
-		name: 'NaPoM',
-		youtube: 'https://www.youtube.com/user/NaPoMBeatbox/featured',
-		image: ''
-	},
-	{
-		name: 'Alexinho',
-		youtube: 'https://www.youtube.com/channel/UCXXGewmKCnsc4WiS3_4hZig/featured',
-		image: ''
-	},
-	{
-		name: 'TylaDubya',
-		youtube: 'https://www.youtube.com/user/TylaDubya/featured',
-		image: ''
-	},
-	{
-		name: 'ALEM',
-		youtube: 'https://www.youtube.com/user/38mael/featured',
-		image: ''
-	},
-	{
-		name: 'Azel',
-		youtube: 'https://www.youtube.com/user/azelbeatbox/featured',
-		image: ''
-	},
+	// {
+	// 	name: 'Audical',
+	// 	youtube: 'https://www.youtube.com/channel/UCf7ujqQzI9YY5RzzBgpGSbg/featured',
+	// 	image: ''
+	// },
+	// {
+	// 	name: 'NaPoM',
+	// 	youtube: 'https://www.youtube.com/user/NaPoMBeatbox/featured',
+	// 	image: ''
+	// },
+	// {
+	// 	name: 'Alexinho',
+	// 	youtube: 'https://www.youtube.com/channel/UCXXGewmKCnsc4WiS3_4hZig/featured',
+	// 	image: ''
+	// },
+	// {
+	// 	name: 'TylaDubya',
+	// 	youtube: 'https://www.youtube.com/user/TylaDubya/featured',
+	// 	image: ''
+	// },
+	// {
+	// 	name: 'ALEM',
+	// 	youtube: 'https://www.youtube.com/user/38mael/featured',
+	// 	image: ''
+	// },
+	// {
+	// 	name: 'Azel',
+	// 	youtube: 'https://www.youtube.com/user/azelbeatbox/featured',
+	// 	image: ''
+	// },
 ];
 var challenges = [
 	{
 		name: 'Bass',
 		status: false,
-		level: 'basics'
+		level: 'basics',
+		tutorial: ''
 	},
 		{
 		name: 'Hi-Hat',
 		status: false,
-		level: 'basics'
+		level: 'basics',
+		tutorial: ''
 	},
 		{
 		name: 'Snare',
 		status: false,
-		level: 'basics'
+		level: 'basics',
+		tutorial: ''
 	},
 		{
 		name: 'Bass, Hi-Hat and Snare',
 		status: false,
-		level: 'basics'
+		level: 'basics',
+		tutorial: ''
 	},
 		{
 		name: 'Throat-Bass',
 		status: false,
-		level: 'pre-intermediate'
+		level: 'pre-intermediate',
+		tutorial: ''
 	},
 		{
 		name: 'Vibration-Bass',
 		status: false,
-		level: 'pre-intermediate'
+		level: 'pre-intermediate',
+		tutorial: ''
 	},
 		{
 		name: 'Lip Roll',
 		status: false,
-		level: 'pre-intermediate'
+		level: 'pre-intermediate',
+		tutorial: ''
 	},
 		{
 		name: 'Lip Roll, Hi-Hat and Bass',
 		status: false,
-		level: 'Intermediate'
+		level: 'Intermediate',
+		tutorial: ''
 	},
 		{
 		name: 'Inward-Bass',
 		status: false,
-		level: 'Intermediate'
+		level: 'Intermediate',
+		tutorial: ''
 	},
 		{
 		name: 'Inward-Lip Roll',
 		status: false,
-		level: 'Advanced'
+		level: 'Advanced',
+		tutorial: ''
 	},
 		{
 		name: 'Tongue Bass',
 		status: false,
-		level: 'Advanced'
+		level: 'Advanced',
+		tutorial: ''
 	},
 ];
 
 
+var newPage = function() {
+	$('#page').html('');
+}
+
 function display() {
 	return {
 		videosDisplay: function() {
-			for (var i = 0; i < generalVideos.length; i++) {
-				$('#generalVideos').append(generalVideos[i]);
-			};
+			generalVideos.forEach(function(element) {
+				$('#generalVideos').append(element);
+			})
+		},
+		beatBoxersDisplay: function() {
+			beatBoxers.forEach(function(element) {
+				$('<img class="bbImages">').appendTo('#beatboxers').attr('src', element.image);
+			})
+		},
+		challenges: function() {
+			$('#challenges').click(function() {
+				newPage();
+				$('<div id="challengesDiv"> </div>').appendTo('#page').append('<ul id="challengesList"> </ul>');
+				//refactor to foreach
+				challenges.forEach(function(element) {
+					$('#challengesList').append('<li class="challengesLi">' + element.name + ' ' + '(' + element.level + ')' + '</li>');
+				})
+			});
 		}
 	}
 }
 
 var display = display();
 $('#videos').html(display.videosDisplay());
+$('#beatboxers').append(display.beatBoxersDisplay());
+$('#challenges').html(display.challenges());
+
+
+	$('.challengesLi').click(function() {
+		alert('hi this is challenge ')
+	})
